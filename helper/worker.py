0bb36ffee312addaabd057270378f1d10d447b6e
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-06-08 20:00:07 +0530

    Update worker.py

--- helper/worker.py
+++ helper/worker.py
@@ -87,7 +87,7 @@ async def encc(e):
         er = stderr.decode()
         try:
             if er:
-                await e.edit(str(er) + "\n\n**ERROR** Contact @danish_00")
+                await e.edit(str(er) + "\n\n**ERROR** Contact @AidanNia")
                 COUNT.remove(e.chat_id)
                 os.remove(dl)
                 return os.remove(out)
@@ -157,7 +157,7 @@ async def sample(e):
     er = stderr.decode()
     try:
         if er:
-            await e.edit(str(er) + "\n\n**ERROR** Contact @danish_00")
+            await e.edit(str(er) + "\n\n**ERROR** Contact @AidanNia")
             COUNT.remove(e.chat_id)
             os.remove(dl)
             os.remove(out)
